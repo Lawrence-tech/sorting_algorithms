2ad04fe2b782ccfc8062c3a2a414bdf095f5246e
Epicsociety <measkswho@gmail.com>
2023-02-20 12:36:04 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,12 @@
 #include "sort.h"
 
+void swap(int *ptr1, int *ptr2)
+{
+	int temp = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = temp;
+}
+
 
 /**
  * bubble_sort - sorts an array of integers in ascending order
@@ -10,5 +17,17 @@
 */
 void bubble_sort(int *array, size_t size)
 {
+	long unsigned int i, j;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i -1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
