6f031b07a86eb8e4b1c150b32bdf43c06e3dba72
Epicsociety <measkswho@gmail.com>
2023-02-20 10:51:57 +0300

    a skeleton of 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @*array: pointer to the array to be sorted
+ * @size: size of the array
+ *
+ * Return: nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+
+}
